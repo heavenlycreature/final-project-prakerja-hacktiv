d13469ea64dcf04019cc171032ac77db7a8efbe5
heavenlycreature <lepiinnn@gmail.com>
2023-09-21 23:13:02 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav ul');
+const navLink = document.querySelectorAll('nav ul li');
+
+burger.addEventListener('click', (e) => {
+    nav.classList.toggle('active');
+    burger.classList.toggle('menu-active');
+});
+
